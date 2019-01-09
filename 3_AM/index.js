847100d13b7c8fb803d855fc3dad4d5a2fa44e19
AARON <misteraaronchen@gmail.com>
2019-01-09 12:56:36 -0500

    3_AM website

--- 3_AM/index.js
+++ 3_AM/index.js
@@ -1,11 +1,25 @@
 const express = require("express");
+const path = require('path')
 const app = express();
+app.use(express.static('public'));
 
 app.use(express.static(__dirname + "/View"));
 app.use(express.static(__dirname + "/Script"));
 
 app.get("/", (req, res) => {
-  res.sendFile("index.html");
+  res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname + '/pokemon_trainer.html'));
+});
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname + '/pichu.html'));
+});
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname + '/bowser.html'));
 });
 
 app.listen(3000);
